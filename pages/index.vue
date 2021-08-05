57ee057f4a443d8dfcf94193d6d5240fc86b7f56
Tianxiang Ying <896267775@qq.com>
2021-08-05 16:21:21 +0800

    Init

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="wrap">
+  <div v-for="(item, index) in arr" :key="index">
+    {{item}}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      arr: "hello, world!".split("")
+    }
+  }
+}
+</script>
+
+<style>
+.wrap{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  font-size: 50px;
+  align-items: center;
+  height: 100vh;
+}
+
+.wrap div{
+  width:100px;
+}
+</style>
