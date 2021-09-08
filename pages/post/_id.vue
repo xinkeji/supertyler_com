b8ea40de7f57d436b55e5bf639efc8086f8602da
SuperTyler <896267775@qq.com>
2021-09-09 05:19:38 +0800

    添加vue-swiper.js

---
+++ pages/post/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+    {{ $route.params.id }}
+  </view>
+</template>
+
+<script>
+export default {
+  validate({params}) {
+    return /^\d+$/.test(params.id);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
